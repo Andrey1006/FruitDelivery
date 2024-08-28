b3fa31ba2ab9016716c6f467981156a3be637dc6
Andrey1006 <storozhko1999@gmail.com>
2024-08-28 13:37:30 +0300

    rename some files

---
+++ FruitExpress/Sources/Modules/MainViewController/ProductCollectionCell/ProductCellViewLayout .swift
@@ -0,0 +1,17 @@
+//
+//  ProductCellViewLayout .swift
+//  FruitExpress
+//
+//  Created by Андрей Сторожко on 15.08.2024.
+//
+
+import Foundation
+import UIKit
+
+struct ProductCellViewLayout {
+    let contentInsets: UIEdgeInsets = .init(top: 8, left: 24, bottom: 16, right: 24)
+    let buttonHeight: CGFloat = 40
+    let titleFont: UIFont = UIFont.systemFont(ofSize: 12, weight: .regular)
+    let buttonTextFont: UIFont = UIFont.systemFont(ofSize: 21, weight: .heavy)
+    let spacingBetweetImageAndTitle: CGFloat = 12
+}
